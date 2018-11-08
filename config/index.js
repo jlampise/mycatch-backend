bee5aa2d839ed84e641d10fad1a7205a65562e1c
Jussi Lampiselka <jlampise@iki.fi>
2018-11-08 12:49:49 +0200

    Add production db.connection

--- config/index.js
+++ config/index.js
@@ -1,6 +1,12 @@
 const bunyan = require('bunyan');
 
+
+let dbConnection = 'mongodb://localhost:27017/mycatchdb';
+
+if (process.env.NODE_ENV === 'production') {
+  dbConnection = process.env.MONGO_URI;
+}
 module.exports = {
   logger: bunyan.createLogger({ name: 'MyCatch' }),
-  dbConnection: 'mongodb://localhost:27017/mycatchdb'
+  dbConnection
 };
